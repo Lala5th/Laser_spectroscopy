dcd5fab45c67fc0e86a636ac8e389e1bf4236a2b
Lala5th <lala5th@gmail.com>
2020-10-13 21:13:01 +0100

    Added basic analysis code

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ This repository contains the datasets and analysis code for the 3rd Year laser s
 Datafiles are located in the data folder. Current files and short descriprors:
 - PD1_1.csv/PD2_1.csv: Oscilloscope output of PD1/PD2 with the doppler broadened spectrum.
 - PD2_2.csv: Oscilloscope output of PD2 with the pump beam introduced into the system. PD2 is the probe beam. PD1's output is lost due to an error.
+
+# Analysis code
+
+The analysis code included in the repository and their usages are:
+- `plot_rough_data.py dataset`: Plots the rough data with a moving average applied to it, as well as allows fitting to it using the `fit_line(xmin,xmax)` method afterwards.
---
+++ plot_rough_data.py
@@ -0,0 +1,56 @@
+#!/usr/bin/ipython3
+
+import sys
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from scipy.optimize import curve_fit
+
+args = sys.argv
+argnum = 2
+usage = '''Usage:
+.\{0} dataset
+or in ipython console:
+%run {0} dataset'''.format(args[0])
+
+if len(args) != argnum:
+    print(usage)
+    sys.exit()
+
+fitfunc = lambda x, a, b, A, m, s: a*x + b - A*gaussian(x,m,s)
+
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+def fit_line(xmin,xmax):
+    x = []
+    y = []
+    for i in range(data['t'].size):
+        if(data['t'][i] > xmin and data['t'][i] < xmax):
+            x.append(data['t'][i])
+            y.append(data['I'][i])
+        elif(data['t'][i] >= xmax):
+            break
+    x = np.array(x)
+    y = np.array(y)
+    fit, cov = curve_fit(fitfunc,x,y,p0=[50,0,1,(xmin+xmax)/2,0.001])
+    plt.plot(x,fitfunc(x,*fit))
+    for param in zip(fit,sp.sqrt(np.diag(cov))):
+        print(param[0],'+-',param[1])
+
+gaussian = lambda x,m,s : norm.pdf(x,m,s)
+
+data = np.loadtxt(args[1],dtype=[('t',np.float64,()),('I',np.float64,())],skiprows=1,delimiter=',')
+#def fitfunc(x,B,M1,M2,M3,M4,A1,A2,A3,A4,S,T0):
+#    return(B-A1*gaussian(x,T0-M1,S)-A1*gaussian(x,T0-M2,S)-A1*gaussian(x,T0-M3,S)-A1*gaussian(x,T0-M4,S)-A1*gaussian(x,T0+M1,S)-A1*gaussian(x,T0+M2,S)-A1*gaussian(x,T0+M3,S)-A1*gaussian(x,T0+M4,S)).flatten()
+
+#fit, cov = curve_fit(fitfunc,data['t'],data['I'],p0=[48,0.0034,0.0088,0.0217,0.032,1,2,1,0.25,0.001,-0.0034])
+
+plt.ion()
+plt.plot(data['t'][5:-4],moving_average(data['I'],n=10))
+plt.xlabel("Time [s]")
+plt.ylabel("Intensity [a.u.]")
+plt.show()
